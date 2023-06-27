b51818e31d6ac3cccade3b71009aefde7bb360e4
Brandon Kilgore <brandonk1023@gmail.com>
2023-06-27 12:22:18 -0400

    added code into the database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,6 +16,18 @@ const initdb = async () =>
 export const putDb = async (content) => console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  
+  const editorDb = await openDB('jate', 1);
+  const tx = editorDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(id);
+
+  const result = await request;
+  if (!result) {
+    console.log('No data available')
+  } else {
+    return result.value;
+  }
 
 initdb();
